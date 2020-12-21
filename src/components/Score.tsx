42a629e83b544120b89013809f6eb4fe205fbe2f
joshua-augustinus <joshua.augustinus@gmail.com>
2020-12-21 12:38:10 +1100

    Working animation

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -1,14 +1,57 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Animated, StyleSheet, Text, TouchableOpacity, useWindowDimensions, View } from "react-native";
+import { Animated, Easing, StyleSheet, Text, TouchableOpacity, useWindowDimensions, View } from "react-native";
 
 interface Props {
     value: string
 }
 
-export const SCORE_HEIGHT = 30;
+const SCORE_HEIGHT = 30;
+const SCORE_WIDTH = 100;
+const PULSE_X = 1.3
+const PULSE_Y = 2
+
+const ANIMATION_ARRAY_SIZE = 13; //Must be odd number
+
+/**
+ * Setup input range for pulse animation
+ */
+let pulseInputRange = [];
+for (let i = 0; i < ANIMATION_ARRAY_SIZE; i++) {
+    pulseInputRange.push(i);
+}
+
+/**
+ * Setup output range for pulse animation
+ */
+let pulseOutputRangeY = [];
+for (let i = 0; i < ANIMATION_ARRAY_SIZE; i++) {
+    if (i % 2 === 0) {
+        pulseOutputRangeY.push(1);
+    } else {
+        pulseOutputRangeY.push(PULSE_Y);
+
+    }
+}
+
+
+/**
+ * Setup output range for pulse animation
+ */
+let pulseOutputRangeX = [];
+for (let i = 0; i < ANIMATION_ARRAY_SIZE; i++) {
+    if (i % 2 === 0) {
+        pulseOutputRangeX.push(1);
+    } else {
+        pulseOutputRangeX.push(PULSE_X);
+
+    }
+}
+
+
 
 const Score = (props: Props) => {
     const animationState = useRef(new Animated.Value(0)).current;
+    const pulseAnimationState = useRef(new Animated.Value(0)).current;
     const [oldValue, setOldValue] = useState("0.0");
 
     useEffect(() => {
@@ -31,9 +74,41 @@ const Score = (props: Props) => {
 
     }, [props.value])
 
+    useEffect(() => {
+        //on value changed
+        const pulse = Animated.timing(pulseAnimationState, {
+            useNativeDriver: true,
+            toValue: ANIMATION_ARRAY_SIZE,
+            duration: 5000,
+            easing: Easing.linear
+        });
+
+        pulse.start(() => {
+            Animated.timing(pulseAnimationState, {
+                useNativeDriver: true,
+                toValue: 0,
+                duration: 0
+            }).start();
+        });
+
+
+
+
+    }, [props.value])
+
     /**
-     * Animation for the actual text
+     * Animation for pulse
      */
+    const scaleX = pulseAnimationState.interpolate({
+        inputRange: pulseInputRange,
+        outputRange: pulseOutputRangeX
+    })
+    const scaleY = pulseAnimationState.interpolate({
+        inputRange: pulseInputRange,
+        outputRange: pulseOutputRangeY
+    })
+    const pulseTransform = [{ scaleX: scaleX }, { scaleY: scaleY }];
+
 
 
     /**
@@ -57,15 +132,26 @@ const Score = (props: Props) => {
 
     return (
 
+        <View>
+            <Animated.View style={{ ...styles.pulse, transform: pulseTransform }} >
 
-        <View style={styles.container} >
-            <View style={StyleSheet.absoluteFill}>
-                <Animated.Text style={{ transform: transform2 }}>{props.value}</Animated.Text>
-            </View>
-            <View style={StyleSheet.absoluteFill}>
-                <Animated.Text style={{ transform: transform3 }}>{oldValue}</Animated.Text>
+            </Animated.View>
+            <View style={styles.container} >
+
+                <View style={StyleSheet.absoluteFill}>
+                    <View style={styles.border} />
+                </View>
+
+                <View style={StyleSheet.absoluteFill}>
+                    <Animated.Text style={{ transform: transform2 }}>{props.value}</Animated.Text>
+                </View>
+                <View style={StyleSheet.absoluteFill}>
+                    <Animated.Text style={{ transform: transform3 }}>{oldValue}</Animated.Text>
+                </View>
+
             </View>
         </View>
+
     )
 }
 
@@ -73,10 +159,22 @@ export { Score }
 
 const styles = StyleSheet.create({
     container: {
-        width: 100,
+        width: SCORE_WIDTH,
         height: SCORE_HEIGHT,
+        overflow: 'hidden',
+        ...StyleSheet.absoluteFillObject
+    },
+    pulse: {
+        backgroundColor: 'green',
+        height: SCORE_HEIGHT,
+        width: SCORE_WIDTH,
+    },
+
+    border: {
         borderWidth: 1,
         borderColor: 'blue',
+        height: SCORE_HEIGHT,
+        backgroundColor: 'white'
     },
     text: {
     }
